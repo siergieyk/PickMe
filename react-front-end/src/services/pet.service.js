2581c682783593bae9aed569a0be21a0ed60feca
siergieyk <46364097+siergieyk@users.noreply.github.com>
2020-09-14 15:34:06 -0700

    Update pet.service.js

--- react-front-end/src/services/pet.service.js
+++ react-front-end/src/services/pet.service.js
@@ -9,24 +9,30 @@ class PetDataService {
     return http.get(`/pets/${id}`);
   }
 
+
   create(data) {
     return http.post("/pets", data);
   }
+
 
   update(id, data) {
     return http.put(`/pets/${id}`, data);
   }
 
+
   delete(id) {
     return http.delete(`/pets/${id}`);
   }
+
 
   deleteAll() {
     return http.delete(`/pets`);
   }
 
+
   findByTitle(title) {
     return http.get(`/pets?title=${title}`);
+    
   }
 }
 
